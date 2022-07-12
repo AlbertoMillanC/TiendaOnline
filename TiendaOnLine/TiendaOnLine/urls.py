46b97c5bdfee2b69048271e9d86ca1fdb0c942f0
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-07-12 04:13:41 -0500

    se implementa la vista get del forulario buscar

--- TiendaOnLine/TiendaOnLine/urls.py
+++ TiendaOnLine/TiendaOnLine/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from gestionPedidos import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('busqueda/', views.busqueda_productos, name='busqueda_productos'),
+    path('busqueda/', views.busqueda_productos),
+    path('buscar/', views.buscar),
 ]
--- TiendaOnLine/gestionPedidos/views.py
+++ TiendaOnLine/gestionPedidos/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def busqueda_productos(request):
+    
     return render(request, 'busqueda_productos.html')
+
+
+
+
+
+
+def buscar(request):
+    
+    mensaje= "articulos encontrados %r" %request.GET['prd']
+    
+    return HttpResponse(mensaje)
+    
     
