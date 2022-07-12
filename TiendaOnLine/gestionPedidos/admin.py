dd378ed5a9958b223d9493cf52562a971f8d2976
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-07-12 02:03:33 -0500

    se agregan filtros de busqueda

--- TiendaOnLine/gestionPedidos/admin.py
+++ TiendaOnLine/gestionPedidos/admin.py
@@ -1,3 +1,4 @@
+from calendar import c
 from re import A
 from django.contrib import admin
 from gestionPedidos.models import Client,Product,Order
@@ -5,13 +6,17 @@ from gestionPedidos.models import Client,Product,Order
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('id','name','surname','email','phone','address','city','country','date_created','date_updated')
     search_fields = ('id','name','surname','email','phone','address','city','country','date_created','date_updated')
+    list_filter = ('id','name','surname','email','phone','address','city','country','date_created','date_updated')
+    
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id','name','description','price','date_created','date_updated')
     search_fields = ('id','name','description','price','date_created','date_updated')
-    
+    list_filter = ('price','name','date_created','date_updated') 
+     
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id','client','date_created','date_updated','order_status')
     search_fields = ('id','client','date_created','date_updated','order_status')
+    list_filter = ('client','date_created','date_updated','order_status')
 
 
 admin.site.register(Client,ClientAdmin)
