9f0d8b3dd45ac31789025799e88c1fb5c044eeb7
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-07-12 01:49:43 -0500

    se generan busquedas en el panel de administración, se agregan los campos de busqueda y se colocan nombres en español a los campos de busqueda

--- TiendaOnLine/gestionPedidos/admin.py
+++ TiendaOnLine/gestionPedidos/admin.py
@@ -2,9 +2,21 @@ from re import A
 from django.contrib import admin
 from gestionPedidos.models import Client,Product,Order
 
-admin.site.register(Client)
-admin.site.register(Product)
-admin.site.register(Order)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id','name','surname','email','phone','address','city','country','date_created','date_updated')
+    search_fields = ('id','name','surname','email','phone','address','city','country','date_created','date_updated')
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','name','description','price','date_created','date_updated')
+    search_fields = ('id','name','description','price','date_created','date_updated')
+    
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','client','date_created','date_updated','order_status')
+    search_fields = ('id','client','date_created','date_updated','order_status')
+
+
+admin.site.register(Client,ClientAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Order,OrderAdmin)
 
 
 
--- TiendaOnLine/gestionPedidos/models.py
+++ TiendaOnLine/gestionPedidos/models.py
@@ -5,27 +5,28 @@ from django.db import models
 # Create your models here.Cli
 
 class Client(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    phone = models.CharField(max_length=12)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=20)
-    country = models.CharField(blank=True,null=True,max_length=20)
-    date_created = models.DateField(default=date.today)
-    date_updated = models.DateField(default=date.today)
+    name = models.CharField(max_length=50,verbose_name='Nombre')
+    surname = models.CharField(max_length=50,verbose_name='Apellido')
+    email = models.EmailField(max_length=50,verbose_name='Email')
+    phone = models.CharField(max_length=12,verbose_name='Telefono')
+    address = models.CharField(max_length=50,verbose_name='Direccion')
+    city = models.CharField(max_length=20,verbose_name  = 'Ciudad')
+    country = models.CharField(blank=True,null=True,max_length=20,verbose_name='Pais')
+    date_created = models.DateField(default=date.today,verbose_name='Fecha de creacion')
+    date_updated = models.DateField(default=date.today,verbose_name='Fecha de actualizacion')
     
+        
     def __str__(self):
         return ' id %s client %s  %s' % (self.id,self.name,self.surname)
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    date_created = models.DateField(default=date.today)
-    date_updated = models.DateField(default=date.today)
+    name = models.CharField(max_length=50,verbose_name='producto')
+    description = models.CharField(max_length=50,verbose_name='Descripcion')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Precio')
+    date_created = models.DateField(default=date.today,verbose_name='Fecha de creacion')
+    date_updated = models.DateField(default=date.today,verbose_name='Fecha de actualizacion')
     
     def __str__(self):
-        return 'the product is %s the description is %s the price is %s the date created is %s the date updated is %s' % (self.name, self.description, self.price, self.date_created, self.date_updated)
+        return 'Produc: %s  %s  %s  %s %s %s' % (self.id, self.name, self.description, self.price, self.date_created, self.date_updated)
     
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
@@ -34,6 +35,6 @@ class Order(models.Model):
     order_status = models.BooleanField(default=False)
     
     def __str__(self):
-        return 'the order is %s the client is %s the date created is %s the date updated is %s the order status is %s' % (self.id, self.client, self.date_created, self.date_updated, self.order_status)
+        return ' %s  %s  %s  %s  %s' % (self.id, self.client, self.date_created, self.date_updated, self.order_status)
         
     
