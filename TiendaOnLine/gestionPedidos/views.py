9676decc3f6c2129fd26ce2dc9d640317aa45f40
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-07-13 18:03:42 -0500

    se integra db a vista

--- TiendaOnLine/gestionPedidos/views.py
+++ TiendaOnLine/gestionPedidos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Product
 
 # Create your views here.
 def busqueda_productos(request):
@@ -7,13 +8,18 @@ def busqueda_productos(request):
     return render(request, 'busqueda_productos.html')
 
 
-
-
-
-
 def buscar(request):
     
-    mensaje= "articulos encontrados %r" %request.GET['prd']
+    if request.GET["prd"]:
+        
+      
+      articulo=request.GET["prd"]
+      articulos = Product.objects.filter(name__icontains= articulo)
+  
+      return render(request,'resultado.html',{"Producto":articulos,"query":articulo})
+       
+    else:
+        mensaje= "No se ha introducido ningun articulo"
     
     return HttpResponse(mensaje)
     
