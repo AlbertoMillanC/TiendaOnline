f9ed124513153a9feb43407299b0059fbea0937b
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-07-16 17:02:09 -0500

    dando formato

--- TiendaOnLine/gestionPedidos/views.py
+++ TiendaOnLine/gestionPedidos/views.py
@@ -1,40 +1,41 @@
 from multiprocessing.connection import Client
 from django.shortcuts import render
 from django.http import HttpResponse
-from gestionPedidos.models import Client,Product,Order
+from gestionPedidos.models import Client, Product, Order
 
 # Create your views here.
+
+
 def busqueda_productos(request):
-    
+
     return render(request, 'busqueda_productos.html')
 
 
 def buscar(request):
-    
-    if request.GET["prd"]:
-        
-      
-      articulo=request.GET["prd"]
-      
-      if len(articulo)>20:
-         
-         mensaje="El articulo no puede tener mas de 20 caracteres"
-      else:
-      
-         articulos = Product.objects.filter(name= articulo)
-  
-         return render(request,'resultado.html',{"Product":articulos,"query":articulo})
-       
-    else:
-        mensaje= "No se ha introducido ningun articulo"
-    
-    return HttpResponse(mensaje)
-    
-def contacto(request):
-    
-    if request.method == 'POST':
-        
-        return render(request,'gracias.html',{'mensaje':'Mensaje enviado correctamente'})
-    
-    return render(request, 'contacto.html')    
 
+    if request.GET["prd"]:
+
+      articulo = request.GET["prd"]
+
+      if len(articulo) > 20:
+
+         mensaje = "El articulo no puede tener mas de 20 caracteres"
+      else:
+
+         articulos = Product.objects.filter(name=articulo)
+
+         return render(request, 'resultado.html', {"Product": articulos, "query": articulo})
+
+    else:
+        mensaje = "No se ha introducido ningun articulo"
+
+    return HttpResponse(mensaje)
+
+
+def contacto(request):
+
+    if request.method == 'POST':
+
+        return render(request, 'gracias.html', {'mensaje': 'Mensaje enviado correctamente'})
+
+    return render(request, 'contacto.html')
