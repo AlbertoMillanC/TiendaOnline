3124387b836f0c5e1bdc18caebd033db8fbe5887
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-07-16 03:42:30 -0500

    se agregan mensajes de cantidad de caracteres e input de busqueda en blanco

--- TiendaOnLine/gestionPedidos/views.py
+++ TiendaOnLine/gestionPedidos/views.py
@@ -15,9 +15,15 @@ def buscar(request):
         
       
       articulo=request.GET["prd"]
-      articulos = Product.objects.filter(name= articulo)
+      
+      if len(articulo)>20:
+         
+         mensaje="El articulo no puede tener mas de 20 caracteres"
+      else:
+      
+         articulos = Product.objects.filter(name= articulo)
   
-      return render(request,'resultado.html',{"Product":articulos,"query":articulo})
+         return render(request,'resultado.html',{"Product":articulos,"query":articulo})
        
     else:
         mensaje= "No se ha introducido ningun articulo"
