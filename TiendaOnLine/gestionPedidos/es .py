edc5ce058a67132124c7af3b366482d751eb9806
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-07-25 01:38:57 -0500

    revision de bd

---
+++ TiendaOnLine/gestionPedidos/es .py
@@ -0,0 +1,15 @@
+#  funcion para sumar tres numeros y devolver el resultado
+
+"""def sumar(num1, num2, num3):
+    return num1 + num2 + num3
+
+
+print(sumar(144444444, 5444444442, 3))"""
+
+class coche:
+         def sumar(color,puerta):
+             color = color
+             puerta = 4
+             agregarPuerta = +1
+             puertas = agregarPuerta + puerta          
+             print(coche)
